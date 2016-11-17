0cfe1e658c8dd5382000f8bc533652a771ff1382
Matthieu Segret <matthieu.segret@gmail.com>
2016-11-17 23:31:38 +0100

    Reorganize AllPosts component

--- client/app/containers/posts/AllPosts.js
+++ client/app/containers/posts/AllPosts.js
@@ -1,7 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 
-import Loading from 'components/Loading';
-import PostPreview from 'containers/posts/_PostPreview';
+import ListPosts from 'containers/posts/_ListPosts';
 import withPosts from 'queries/posts/postsQuery';
 
 class AllPosts extends Component {
@@ -10,42 +9,12 @@ class AllPosts extends Component {
     loadMorePosts: PropTypes.func
   }
 
-  constructor(props) {
-    super(props);
-    this.postsPreview = this.postsPreview.bind(this);
-  }
-
-  postsPreview() {
-    const { posts } = this.props.data;
-    if (!posts) { return null; }
-    return posts.map((post) => {
-      return <PostPreview key={post.id} post={post} />;
-    });
-  }
-
   render() {
     const { data: { posts, postsCount, loading }, loadMorePosts } = this.props;
     return (
       <div className="all-posts">
         <h1>Listing posts</h1>
-        <table className="table table-bordered table-striped">
-          <thead>
-            <tr>
-              <th>Title</th>
-              <th>User</th>
-              <th>Created at</th>
-            </tr>
-          </thead>
-
-          <tbody>
-            {this.postsPreview()}
-          </tbody>
-        </table>
-
-        {loading ? <Loading /> : null}
-
-        {(posts && posts.length < postsCount) ?
-          <button className="btn btn-sm btn-default center-block load-more" onClick={loadMorePosts}>Load more</button> : null}
+        <ListPosts posts={posts} postsCount={postsCount} loading={loading} loadMorePosts={loadMorePosts} />
       </div>
     );
   }
