bb4d81fc6fceb7b825885609adcc06adecc04021
Matthieu Segret <matthieu.segret@gmail.com>
2016-12-20 11:15:17 +0100

    Move post form outside new post component

--- client/app/containers/posts/NewPost.js
+++ client/app/containers/posts/NewPost.js
@@ -1,67 +1,23 @@
 import React, { Component, PropTypes } from 'react';
 import { Link } from 'react-router';
-import { reduxForm, Field, SubmissionError } from 'redux-form';
 
-import RenderField from 'components/form/RenderField';
-import Button from 'components/form/Button';
+import PostForm from 'containers/posts/_PostForm';
 import withCreatePost from 'mutations/posts/createPostMutation';
-import withFlashMessage from 'components/withFlashMessage';
 
 class NewPost extends Component {
   static propTypes = {
-    createPost: PropTypes.func,
-    handleSubmit: PropTypes.func,
-    redirect: PropTypes.func,
-    error: PropTypes.func
-  }
-
-  constructor(props) {
-    super(props);
-    this.state = { loading: false };
-    this.submitForm = this.submitForm.bind(this);
-  }
-
-  submitForm(values) {
-    this.setState({ loading: true });
-    return this.props.createPost(values).then((errors) => {
-      this.setState({ loading: false });
-      console.log(errors);
-      console.log(this.props);
-      if (!errors) {
-        this.props.redirect('/', { notice: 'Post was successfully created' });
-      } else {
-        this.props.error('Please review the problems below:');
-        throw new SubmissionError(errors);
-      }
-    });
+    createPost: PropTypes.func
   }
 
   render() {
-    const { handleSubmit } = this.props;
-    const { loading } = this.state;
-
     return (
       <div>
         <h1>New Post</h1>
-        <form onSubmit={handleSubmit(this.submitForm)}>
-          <Field name="title" component={RenderField} type="text" />
-          <Field name="content" component={RenderField} type="textarea" />
-          <Button loading={loading} value="Create Post" />
-        </form>
+        <PostForm action={this.props.createPost} submitName="Create Post" />
         <Link to="/">Back</Link>
       </div>
     );
   }
 }
 
-function validate(values) {
-  const errors = {};
-  if (!values.title) { errors.title = "can't be blank"; }
-  if (!values.content) { errors.content = "can't be blank"; }
-  return errors;
-}
-
-export default reduxForm({
-  form: 'PostForm',
-  validate
-})(withCreatePost(withFlashMessage(NewPost)));
+export default withCreatePost(NewPost);
--- client/app/mutations/posts/createPostMutation.js
+++ client/app/mutations/posts/createPostMutation.js
@@ -3,44 +3,55 @@ import gql from 'graphql-tag';
 
 import { fragments as PostPreviewFragments } from 'containers/posts/_PostPreview';
 import formatErrors from 'utils/errorsUtils';
+import withFlashMessage from 'components/withFlashMessage';
 
-const CREATE_POST = gql`
-  mutation createPost($title: String, $content: String) {
-    createPost(input: { title: $title, content: $content }) {
-      newPost: post {
-        ...PostPreviewFragment
-      },
-      errors {
-        attribute,
-        message
+export default function (WrappedComponent) {
+  const CREATE_POST = gql`
+    mutation createPost($title: String, $content: String) {
+      createPost(input: { title: $title, content: $content }) {
+        newPost: post {
+          ...PostPreviewFragment
+        },
+        errors {
+          attribute,
+          message
+        }
       }
     }
-  }
-  ${PostPreviewFragments.post}
-`;
+    ${PostPreviewFragments.post}
+  `;
 
-function onResult(response) {
-  return response.errors || formatErrors(response.data.createPost.errors);
-}
-
-export default graphql(CREATE_POST, {
-  props: ({ ownProps, mutate }) => ({
-    createPost(post) {
-      return mutate({
-        variables: { ...post },
-        updateQueries: {
-          posts: (state, { mutationResult, queryVariables }) => {
-            const newPost = mutationResult.data.createPost.newPost;
-            if (!newPost) { return null; }
-            return {
-              posts: [newPost, ...state.posts],
-              postsCount: state.postsCount + 1
-            };
-          }
-        }
-      }).then(onResult.bind(ownProps)).catch((error) => {
-        console.log('oops, there was an error sending the query', error);
-      });
+  function onResult(response) {
+    const errors = response.errors || formatErrors(response.data.createPost.errors);
+    if (!errors) {
+      this.redirect('/', { notice: 'Post was successfully created' });
+    } else {
+      this.error('Please review the problems below:');
     }
-  })
-});
+    return errors;
+  }
+
+  const withCreatePost = graphql(CREATE_POST, {
+    props: ({ ownProps, mutate }) => ({
+      createPost(post) {
+        return mutate({
+          variables: { ...post },
+          updateQueries: {
+            posts: (state, { mutationResult, queryVariables }) => {
+              const newPost = mutationResult.data.createPost.newPost;
+              if (!newPost) { return null; }
+              return {
+                posts: [newPost, ...state.posts],
+                postsCount: state.postsCount + 1
+              };
+            }
+          }
+        }).then(onResult.bind(ownProps)).catch((error) => {
+          ownProps.error("Oops, we're sorry, but something went wrong");
+        });
+      }
+    })
+  });
+
+  return withFlashMessage(withCreatePost(WrappedComponent));
+}
