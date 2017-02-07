451c786c2db727213449db0a75846bdef9a6101b
Matthieu Segret <matthieu.segret@gmail.com>
2017-02-07 12:15:34 +0100

    Fetch last current user fields for user edit form

--- client/app/containers/users/EditUser.js
+++ client/app/containers/users/EditUser.js
@@ -1,12 +1,17 @@
 import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router';
 import { reduxForm, Field } from 'redux-form';
+import gql from 'graphql-tag';
 
+import withUserForEditing from 'queries/users/userForEditingQuery';
 import RenderField from 'components/form/RenderField';
 import Button from 'components/form/Button';
+import Loading from 'components/Loading';
 
 class EditUser extends Component {
   static propTypes = {
+    data: PropTypes.object,
     handleSubmit: PropTypes.func
   }
 
@@ -20,6 +25,9 @@ class EditUser extends Component {
   }
 
   render() {
+    const { data: { loading } } = this.props;
+    if (loading) { return <Loading />; }
+
     return (
       <div className="users-edit">
         <h1>Edit user</h1>
@@ -34,6 +42,29 @@ class EditUser extends Component {
   }
 }
 
-export default reduxForm({
-  form: 'EditUserForm'
-})(EditUser);
+export const fragments = {
+  user: gql`
+    fragment UserForEditingFragment on User {
+      name,
+      email
+    }
+  `
+};
+
+function validate(values) {
+  const errors = {};
+  if (!values.name) { errors.name = "can't be blank"; }
+  if (!values.email) { errors.email = "can't be blank"; }
+  return errors;
+}
+
+export default withUserForEditing(
+  connect(
+    (state, props) => ({
+      initialValues: props.currentUser
+    })
+  )(reduxForm({
+    form: 'EditUserForm',
+    validate
+  })(EditUser))
+);
---
+++ client/app/queries/users/userForEditingQuery.js
@@ -0,0 +1,23 @@
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
+
+import { fragments } from 'containers/users/EditUser';
+
+export default function (WrappedComponent) {
+  const GET_USER_FOR_EDITING = gql`
+    query getUserForEditing {
+      currentUser {
+        ...UserForEditingFragment
+      }
+    }
+    ${fragments.user}
+  `;
+
+  const withUserForEditing = graphql(GET_USER_FOR_EDITING, {
+    options: (ownProps) => ({
+      forceFetch: true
+    })
+  });
+
+  return withUserForEditing(WrappedComponent);
+}
