c724fe4a2ce41d5073a3b4cf34e0c6eb6736555e
Matthieu Segret <matthieu.segret@gmail.com>
2017-11-23 14:29:25 +0100

    Fix migrations

--- db/schema.rb
+++ db/schema.rb
@@ -19,8 +19,8 @@ ActiveRecord::Schema.define(version: 20171019193932) do
     t.string "resource_type", null: false
     t.string "author_type"
     t.integer "author_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
     t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
     t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
